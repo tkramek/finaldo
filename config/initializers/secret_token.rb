933dccd361556ecacb7d00c2be8a49c601105da2
Tomek <kramek.tomasz@gmail.com>
2013-05-10 15:44:04 +0200

    Finaldo start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finaldo::Application.config.secret_token = 'd074b4ff6fd9546a037fd382fd7219a517ebdcc9c4894526deaa0b2f3550c5ff55ba632fe1189b66237e76c4fcdae0b1493941c2c0b7cb3b5f8238e59b013ac5'
