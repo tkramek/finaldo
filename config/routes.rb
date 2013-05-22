77088fbbe1b6f7fc9348c5177b5d30289b7d96f2
Tomek <kramek.tomasz@gmail.com>
2013-05-22 23:33:02 +0200

    User model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Finaldo::Application.routes.draw do
   
+  devise_for :users
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
