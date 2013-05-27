5fcf79e5f80af120ce073ca7617488f9b798ddc8
Tomek <kramek.tomasz@gmail.com>
2013-05-28 00:40:14 +0200

    redirect after login from home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Finaldo::Application.routes.draw do
   
+  get "users/show"
+
   resources :instruments
 
 
   devise_for :users
-
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
