80509238c17d18ba5b95e587259ea35cab1437ae
Jakub Śliwiński <sliwinski.jakub@gmail.com>
2013-10-11 22:58:52 +0200

    Play with rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,12 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
-  
+
   #has_many :instruments
   has_many :portfolios
   #has_many :instruments, through: :portfolios
-  
-  after_save do
-    p = Portfolio.new
-    p.user_id = self.id
-    p.save
+
+  after_create do
+    self.portfolios.create!
   end
 end
